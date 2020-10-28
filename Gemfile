f64f372adb94604055a81115f1465eca54407890
trungtq-0433 <tran.quang.trung@sun-asterisk.com>
2020-10-28 23:25:40 +0700

    deploy single instance

--- Gemfile
+++ Gemfile
@@ -10,7 +10,8 @@ gem "capistrano", "~> 3.10", require: false
 gem "capistrano3-puma"
 gem "capistrano-bundler", "~> 2.0"
 gem "capistrano-rails", "~> 1.6", require: false
-gem "capistrano-rvm"
+gem "capistrano-rbenv", "~> 2.2"
+gem "dotenv-rails"
 gem "jbuilder", "~> 2.7"
 gem "mysql2", ">= 0.4.4"
 gem "puma", "~> 4.1"
@@ -19,11 +20,14 @@ gem "sass-rails", ">= 6"
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
 gem "webpacker", "~> 4.0"
 
+# add gem for issue https://github.com/net-ssh/net-ssh/issues/565
+gem "bcrypt_pbkdf", ">= 1.0", "< 2.0"
+gem "ed25519", ">= 1.2", "< 2.0"
+
 group :development, :test do
   gem "brakeman", require: false
   gem "bundler-audit"
   gem "byebug", platforms: %i[mri mingw x64_mingw]
-  gem "dotenv-rails"
   gem "rspec-rails"
   gem "rubocop", "~> 1.0", require: false
   gem "rubocop-performance", "~> 1.8", ">= 1.8.1", require: false
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,70 @@
-# frozen_string_literal: true
+require_relative "deploy/utils"
 
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
+# common config
 set :application, "sun-ci-cd-v4-rails6-sample"
 set :repo_url, "git@github.com:trungtq-0433/sun-ci-cd-v4-rails6-sample.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
+# https://capistranorb.com/documentation/faq/why-does-something-work-in-my-ssh-session-but-not-in-capistrano/
+# https://capistranorb.com/documentation/advanced-features/ptys/
+# By default Capistrano prefers to start a non-login, non-interactive shell,
+# to try and isolate the environment and make sure that things work as expected,
+# regardless of any changes that might happen on the server side.
 # set :pty, true
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+set :rails_env, ENV["RAILS_ENV"]
+set :deploy_to, "/usr/local/rails_apps/#{fetch :application}"
+set :keep_releases, 5
+set :migration_role, :web_app # https://github.com/capistrano/rails#recommendations
+set :assets_roles, :web_app # Defaults to [:web]
+set :puma_role, :web_app # Defaults to [:app]
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+if ENV["DEPLOY_REF"] # Can input branch or tag. Using DEPLOY_REF=tag cap staging deploy
+  set :branch, ENV["DEPLOY_REF"]
+else
+  raise "Please set $DEPLOY_REF !!!"
+end
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# rbenv config => https://github.com/capistrano/rbenv
+set :rbenv_type, :user
+set :rbenv_ruby, File.read(".ruby-version").strip
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+# config share file. i don't know cannot using append
+# no need `linked_files`, becuz using ENV
+# set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle .bundle public/system public/uploads]
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# default shell environment used during command execution
+# logic compatible gem dotenv => https://github.com/bkeepers/dotenv
+set :default_env, load_env_from_file
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# OPTIONAL config PUMA cluster
+set :puma_threads, [0, 5]
+set :puma_workers, 2
+set :puma_preload_app, true
+set :puma_init_active_record, true
+
+namespace :deploy do # becuz command it `cap <ENV> deploy`
+  desc "=======create database======="
+  task :create_database do
+    on roles(:web_app) do |host|
+      within "#{release_path}" do
+        with rails_env: ENV["RAILS_ENV"] do
+          execute :rake, "db:create"
+        end
+      end
+    end
+  end
+  before :migrate, :create_database
+
+  desc "=======link dotenv=======" # using gem dotenv
+  task :link_dotenv do
+    on roles(:web_app) do
+      upload! "/home/deploy/.env", "/home/deploy/.env" if ENV["UPDATE_ENV"]
+      execute "ln -s /home/deploy/.env #{release_path}/.env"
+    end
+  end
+  after "bundler:install", "deploy:link_dotenv"
+end
